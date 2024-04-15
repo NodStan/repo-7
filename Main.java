5aae684ebf9aea2a28c21c406d6341c84d047ed2
Stanley <stanleyobitunwase@gmail.com>
2024-04-15 13:41:06 +0100

    first commit

---
+++ Main.java
@@ -0,0 +1,91 @@
+package repo7;//public class repo7.Main {
+//    public static void main(String[] args) {
+  //      System.out.println("Hello world!");
+  //      String name = "Michael";
+//        System.out.println(name);
+//        String firstname = "Stanley";
+//        String lastname = "Obitunwase";
+//        int Age = 15;
+//        System.out.println("My name is" + " " + firstname + " " + lastname + " " + "and I am" + " " + Age + " " + "years old");
+//        int test = 39;
+//        int Exam = 57;
+//        System.out.println("My test score is " + test);
+//        System.out.println("My exam score is " + Exam);
+//        System.out.println("The total marks = " + (test + Exam));
+//    }
+//}
+
+//class repo7.Main {
+//    public static void main(String[] args) {
+//        String name = "Stanley Obitunwase";
+//        int Age = 15;
+//        String favfood = "Jollof rice and Chicken";
+//        String Schoolname = "Covenant child academy";
+//        int english = 60;
+//        int maths = 69;
+//        int physics = 79;
+//        int chemistry = 54;
+//        System.out.println("My name is " + name + ". ");
+//        System.out.println("I am " + Age + " years old. ");
+//        System.out.println("My favorite food is " + favfood + ". " );
+//        System.out.println("I completed my secondary school in " + Schoolname + " where I was amongst the top 5 students who scored 250+ in jamb in my school.");
+//        System.out.println("Jamb comprises of 4 subjects so i did English, maths, physics, and chemistry; ");
+//        System.out.println("I scored " + english + " english, " + maths + ", in maths " + physics + ", in physics " + chemistry + ", in chemistry, which together gives a total score of " + (maths + english + chemistry + physics) + ".");
+//        System.out.println("Thanks");
+//        }
+//    }
+
+
+//class repo7.Main {
+//    public static void main(String[] args){
+//        int u = 25;
+//        int a = 10;
+//        int s = 200;
+//        int V = (u * u) + 2 * a * s;
+//        System.out.println(V);
+//    }
+//}
+
+//class repo7.Main {
+//    public static void main(String[] args){
+//        int C = 13;
+//        int F = (((C * 9)/5) + 32);
+//        System.out.println(F);
+//    }
+//}
+
+//class repo7.Main {
+//    public static void main(String[] args){
+//        int a = 5;
+//        a++;
+//        System.out.println(a);
+//    }
+//}
+
+
+//class repo7.Main {
+//    public static void main(String[] args)
+//        if(5>=10){
+//        System.out.println("5 is greater than or equal to 10");
+//    }else {
+//        System.out.println("5 is not greater than or equal to 10");
+//    }
+//}
+
+class Main {
+    public static void main(String[] args){
+       int withdrawal_amount = 50000;
+       int available_amount = 40000;
+       if (withdrawal_amount>=available_amount){
+           System.out.println("Insufficient balance");
+       }else {
+           System.out.println("Withdrawal succesful");
+       }
+    }
+}
+
+
+
+
+
+
